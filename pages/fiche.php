4c7a4dd52b7883a21b1a3bf23e3391516fde10af
Andrianinandroid <raharisonnyavo02@gmail.com>
2025-06-30 16:35:29 +0200

    t

--- pages/fiche.php
+++ pages/fiche.php
@@ -15,7 +15,7 @@ $fiches = get_fiche($_GET['emp_no']);
 <script src="../assets/css/bootstrap/js/bootstrap.bundle.min.js"></script>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">EMPLOYES</a>
+    <a class="navbar-brand" href="#">EMPLOYEES</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
